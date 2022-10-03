e48165fc77bc10f9b069f4d5287cb95ff7c665f7
SerhiiSamara <ndaair@gmail.com>
2022-10-03 22:26:12 +0300

    simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,62 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+
+console.log(SimpleLightbox);
 // Change code below this line
+// const galleryContainer = document.querySelector('.gallery');
+// const imageEl = document.querySelector('.gallery__image');
+
+// galleryContainer.addEventListener('click', onGalleryItemClick);
+
+// const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+// creatGalleryMarkup(galleryMarkup);
+
+// function createGalleryItemsMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+// 		<div class="gallery__item">
+//      <a class="gallery__link" href="${original}">
+//       <img
+//        class="gallery__image"
+//        src="${preview}"
+//        data-source="${original}"
+//        alt="${description}"
+//       />
+//      </a>
+//     </div>`;
+//     })
+//     .join('');
+// }
+
+// function creatGalleryMarkup(galleryMarkup) {
+//   galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+// }
+
+// function onGalleryItemClick(e) {
+//   e.preventDefault();
+//   if (e.target.nodeName !== 'IMG') {
+//     return;
+//   }
+
+//   const options = {
+//     onShow: () => window.addEventListener('keydown', onEscClick),
+//     onClose: () => window.removeEventListener('keydown', onEscClick),
+//   };
+
+//   const instance = basicLightbox.create(
+//     `<img src="${e.target.dataset.source}">`,
+//     options
+//   );
+
+//   instance.show();
+
+//   function onEscClick(e) {
+//     if (e.code === 'Escape') {
+//       instance.close();
+//     }
+//   }
+// }
 
 console.log(galleryItems);
